e6d3b80482c264b58aa547467c1b8f083dc564b8
nanton83 <nicholas.romans@gmail.com>
2020-05-11 11:39:29 -0400

    created nested route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
-      resources :markets
+      resources :markets do
+        resources :items
+      end
     end
   end
 
