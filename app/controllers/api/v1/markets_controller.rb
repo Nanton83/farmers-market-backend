827a49e5e32f2f60a88af2949fcc1b4414315741
nanton83 <nicholas.romans@gmail.com>
2020-05-11 11:21:45 -0400

    markets controller and actions added

--- app/controllers/api/v1/markets_controller.rb
+++ app/controllers/api/v1/markets_controller.rb
@@ -1,2 +1,36 @@
 class Api::V1::MarketsController < ApplicationController
+
+    # Actions...
+
+    def index
+        @markets = Market.all
+        render json: @markets
+    end
+
+    def create
+        @market = Market.new(market_params)
+        if @market.save
+            render json: @market
+        else
+            render json: {error: 'ERROR: Market Not Created'}
+        end
+    end
+
+    def show
+        @market = Market.find(params[:id])
+        render json: @market
+    end
+
+    def destroy
+        @market = Market.find(params[:id])
+        @market.destroy
+    end
+
+    private
+
+    def market_params
+        params.require(:market).permit(:name, :location, :is_open)
+    end
+
+
 end
