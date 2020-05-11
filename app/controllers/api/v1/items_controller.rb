6e1d267ed0cbca16c0233a09fbc905e564b98a7d
nanton83 <nicholas.romans@gmail.com>
2020-05-11 11:14:57 -0400

    items controller and actions created

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::ItemsController < ApplicationController
+    
+    # Actions...
+
+    def index
+        @items = Item.all
+        render json: @items
+    end
+
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+            render json: @item
+        else
+            render json: {error: 'ERROR: Item Not Created'}
+        end
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        render json: @item
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:mareket_id, :name, :description, :price, :amount_available, :unit)
+    end
+
 end
